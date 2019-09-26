b2b81034ded359389daaddd548f731b947151b03
Poul Henriksen <pohe@easj.dk>
2019-09-26 12:45:11 +0200

    Add project files.

---
+++ ArrayStructureEx/InsertCodeHere.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayStructureEx
+{
+    class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            
+
+            // The LAST line of code should be ABOVE this line
+        }
+
+
+
+
+
+
+        // Declare your methods here
+    }
+}
